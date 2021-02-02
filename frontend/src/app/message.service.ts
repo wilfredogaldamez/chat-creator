e9f4b8f93c5a60f227fca054a72e70f8ee1c5cca
Wilfredo Galdamez <wilfredogaldamez1994@gmail.com>
2021-02-01 20:51:47 -0500

    added web request service

---
+++ frontend/src/app/message.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { WebRequestService } from './web-request.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(private webReqService:  WebRequestService) { }
+
+  createChat(title: string){
+    //we want to send a web request to create a chat
+    this.webReqService.post('chat', { title });
+  }
+}
