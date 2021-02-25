87857eca06e2395dae49bfaaa4d1424131914f44
Wilfredo Galdamez <wilfredogaldamez1994@gmail.com>
2021-02-25 14:02:48 -0500

    corrected routing to follow conventions

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ChatComponent } from './pages/chat/chat.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'chat', component: ChatComponent },
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  { path: 'chat', component: HomeComponent },
   { path: 'chat/:chatId', component: ChatComponent },
 ];
 
